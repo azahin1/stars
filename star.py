77a3f1a4b24b951dd0d7f34de65d776a3f5ca7d5
azahin1 <azahin1@ualberta.ca>
2023-04-15 18:34:56 -0600

    star background done

--- star.py
+++ star.py
@@ -2,52 +2,55 @@
 title: Star sprite
 '''
 from sprites import Sprite
+from pygame import K_UP, K_DOWN, K_LEFT, K_RIGHT, K_w, K_a, K_s, K_d
+from random import randint
 from loader import *
 
 class Star(Sprite): # inherits from Sprite class
     def __init__(self, window, x = 0, y = 0):
         Sprite.__init__(self, window, x, y)
-        self.setDimentions(30, 30)
+        self.setDimentions(5, 5)
         self.setColour(STAR_COLOUR)
-        self.spdX = 5 # horrizontal speed
-        self.spdY = 4 # vertical speed
-        self.dirX = 1 # starting horizontal direction
-        self.dirY = -1 # starting vertical direction
         self.destroyed = False
+        self.spd = 5
+        
+    def move(self, keys): # moving the paddle with key presses
+        if keys[K_LEFT] or keys[K_a]: # a key moves it left
+            self.x += self.spd
+        if keys[K_RIGHT] or keys[K_d]: # d key moves it right
+            self.x -= self.spd
+        if keys[K_UP] or keys[K_w]: # w key moves it up
+            self.y += self.spd
+        if keys[K_DOWN] or keys[K_s]: # s key moves it down
+            self.y -= self.spd
 
-    def move(self):
-        self.x += self.spdX * self.dirX # moves it horizontally
-        self.y += self.spdY * self.dirY # moves it vertically
-
-        #-- Bounderies and bouncing
-        if self.x < 0: # left wall
-            self.x = 0
-            self.dirX = 1
+        #-- bounderies
         if self.x > self.window.getWidth() - self.getWidth(): # right wall
+            self.x = 0
+            self.y = randint(0, window.getHeight() - 5)
+        if self.x < 0: # left wall
             self.x = self.window.getWidth() - self.getWidth()
-            self.dirX = -1
-        if self.y < 0: # top wall
-            self.y = 0
-            self.dirY = 1
+            self.y = randint(0, window.getHeight() - 5)
         if self.y > self.window.getHeight() - self.getHeight(): # bottom wall
+            self.y = 0
+            self.x = randint(0, window.getWidth() - 5)
+        if self.y < 0: # top wall
             self.y = self.window.getHeight() - self.getHeight()
-            self.dirY = -1
+            self.x = randint(0, window.getWidth() - 5)
 
-        self.setPOS(self.x, self.y) # changes the position of the ball
-        return self.destroyed
+        self.pos = (self.x, self.y) # moving it based on new values
 
 if __name__ == "__main__":
     from pygame import init
     from window import Window
-    from random import randint
 
     init()
     window = Window()
-    stars = [Star(window, randint(0, window.getWidth() - 30), randint(0, window.getHeight() - 30)) for _ in range(10)]
+    stars = [Star(window, randint(0, window.getWidth() - 5), randint(0, window.getHeight() - 5)) for _ in range(120)]
     while True:
         window.getEvents()
         window.clearScreen()
         for ball in stars:
             window.blitSprite(ball)
-            ball.move()
+            ball.move(window.getKeysPressed())
         window.updateScreen()
