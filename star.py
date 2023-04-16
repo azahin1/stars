e7f97fbe0c57590f87251cb6add648721b7df12a
azahin1 <azahin1@ualberta.ca>
2023-04-15 20:05:35 -0600

    updated stars and its movement

--- star.py
+++ star.py
@@ -7,36 +7,48 @@ from random import randint
 from loader import *
 
 class Star(Sprite): # inherits from Sprite class
-    def __init__(self, window, x = 0, y = 0):
-        Sprite.__init__(self, window, x, y)
-        self.setDimentions(5, 5)
+    def __init__(self, window):
+        Sprite.__init__(self, window)
+        self.setDimentions(2, 2)
+        self.setPOS(randint(0, self.window.getWidth() - self.getWidth()), randint(0, self.window.getHeight() - self.getHeight()))
         self.setColour(STAR_COLOUR)
         self.destroyed = False
-        self.spd = 5
+        self.accValue = 0.5
+        self.friction = -0.1
+        self.velocity = [0, 0]
+        self.accelaration = [0, 0]
         
     def move(self, keys): # moving the paddle with key presses
-        if keys[K_LEFT] or keys[K_a]: # a key moves it left
-            self.x += self.spd
-        if keys[K_RIGHT] or keys[K_d]: # d key moves it right
-            self.x -= self.spd
-        if keys[K_UP] or keys[K_w]: # w key moves it up
-            self.y += self.spd
-        if keys[K_DOWN] or keys[K_s]: # s key moves it down
-            self.y -= self.spd
+        self.accelaration = [0, 0]
+        if keys[K_LEFT] or keys[K_a]:
+            self.accelaration[0] = self.accValue
+        if keys[K_RIGHT] or keys[K_d]:
+            self.accelaration[0] = -self.accValue
+        if keys[K_UP] or keys[K_w]:
+            self.accelaration[1] = self.accValue
+        if keys[K_DOWN] or keys[K_s]:
+            self.accelaration[1] = -self.accValue
+
+        self.accelaration[0] += self.velocity[0] * self.friction
+        self.accelaration[1] += self.velocity[1] * self.friction
+        self.velocity[0] += self.accelaration[0]
+        self.velocity[1] += self.accelaration[1]
+        self.x += self.velocity[0] + self.accelaration[0]/2
+        self.y += self.velocity[1] + self.accelaration[1]/2
 
         #-- bounderies
         if self.x > self.window.getWidth() - self.getWidth(): # right wall
             self.x = 0
-            self.y = randint(0, window.getHeight() - 5)
+            self.y = randint(0, self.window.getHeight() - self.getHeight())
         if self.x < 0: # left wall
             self.x = self.window.getWidth() - self.getWidth()
-            self.y = randint(0, window.getHeight() - 5)
+            self.y = randint(0, self.window.getHeight() - self.getHeight())
         if self.y > self.window.getHeight() - self.getHeight(): # bottom wall
             self.y = 0
-            self.x = randint(0, window.getWidth() - 5)
+            self.x = randint(0, self.window.getWidth() - self.getWidth())
         if self.y < 0: # top wall
             self.y = self.window.getHeight() - self.getHeight()
-            self.x = randint(0, window.getWidth() - 5)
+            self.x = randint(0, self.window.getWidth() - self.getWidth())
 
         self.pos = (self.x, self.y) # moving it based on new values
 
@@ -46,7 +58,7 @@ if __name__ == "__main__":
 
     init()
     window = Window()
-    stars = [Star(window, randint(0, window.getWidth() - 5), randint(0, window.getHeight() - 5)) for _ in range(120)]
+    stars = [Star(window) for _ in range(300)]
     while True:
         window.getEvents()
         window.clearScreen()
