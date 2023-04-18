55d2e51bbde6a8f934b8a5cd86c218535841f43f
azahin1 <azahin1@ualberta.ca>
2023-04-17 20:06:25 -0600

    refactor to be less coupled and better with phases

--- engine.py
+++ engine.py
@@ -17,63 +17,88 @@ class Engine:
         mix.set_num_channels(300)
         self.window = Window() # game window
         self.player = PlayerStar(self.window) # player sprite
-        self.backgroundStars = [Star(self.window) for _ in range(240)]
+        self.backgroundStars = [Star(self.window) for _ in range(300)]
         self.pluckStars = []
         self.anchorStars = [AnchorStar(self.window, i + 1) for i in range(5)]
         self.shootingStars = [ShootingStar(self.window) for _ in range(4)]
         self.frameCount = 0
         self.chordNum = 0
+        self.phase = 1
 
     def run(self):
         while True:
             self.window.getEvents()
-            self.window.clearScreen()
-
-            if not self.frameCount % 60 and len(self.pluckStars) < 30:
-                self.pluckStars.append(PluckStar(self.window))
             
-            #-- Background Stars
-            for star in self.backgroundStars:
-                star.proximity(self.player)
-                star.move(self.window.getKeysPressed())
-                self.window.blitSprite(star)
-    
-            #-- Player Star
-            self.player.setChordNum(self.chordNum)
-            self.player.playSounds()
-            self.window.blitSprite(self.player)
+            { # cheeky pythonic switch statement
+                1: self.phase1
+            }[self.phase]()
 
-            #-- Pluck Stars
-            for pluck in self.pluckStars:
-                pluck.proximity(self.player)
-                pluck.playSounds(self.player)
-                pluck.setChordNum(self.chordNum)
-                pluck.move(self.window.getKeysPressed())
-                self.window.blitSprite(pluck)
-
-            #-- Anchor Stars
-            self.chordNum = 0
-            for anchor in self.anchorStars:
-                anchor.move(self.window.getKeysPressed())
-                self.window.blitSprite(anchor)
-                if anchor.inRange(self.player):
-                    self.chordNum = anchor.getChordNum()
-
-            #-- Shooting Stars
-            for i, shooting in enumerate(self.shootingStars):
-                shooting.proximity(self.player)
-                if not self.frameCount % 250:
-                    shooting.decreaseFreq()
-                if not (self.frameCount + 30*i) % 150 and not shooting.isActive():
-                    shooting.resetTrejectory()
-                if shooting.isActive():
-                    shooting.setChordNum(self.chordNum)
-                    shooting.playSounds(self.player)
-                    shooting.move(self.window.getKeysPressed())
-                    self.window.blitSprite(shooting)
-
-            self.window.updateScreen()
             self.frameCount += 1
+            self.updateScreen()
+
+    def phase1(self):
+        #-- Background Stars
+        for star in self.backgroundStars: # moving background stars
+            star.proximity(self.player)
+            star.move(self.window.getKeysPressed())
+
+        #-- Player Star
+        self.player.setChordNum(self.chordNum)
+        self.player.playSounds()
+
+        #-- Pluck Stars
+        if not self.frameCount % 60 and len(self.pluckStars) < 30: # adds a pluck star every 2 sec
+            self.pluckStars.append(PluckStar(self.window))
+
+        for pluck in self.pluckStars: # moving and playing pluck stars
+            pluck.proximity(self.player)
+            pluck.playSounds(self.player)
+            pluck.setChordNum(self.chordNum)
+            pluck.move(self.window.getKeysPressed())
+
+        #-- Anchor Stars
+        self.chordNum = 0
+        for anchor in self.anchorStars:
+            anchor.move(self.window.getKeysPressed())
+            if anchor.inRange(self.player): # changes chord of the song based on which anchor star is near
+                self.chordNum = anchor.getChordNum()
+
+        #-- Shooting Stars
+        for i, shot in enumerate(self.shootingStars):
+            shot.proximity(self.player)
+            if not self.frameCount % 250: # makes the shooting stars appear more often as time goes on
+                shot.decreaseFreq()
+            if not (self.frameCount + 30*i) % 150 and not shot.isActive(): # resets trajectory when the shooting star is inactive
+                shot.resetTrejectory()
+            if shot.isActive(): # making it do things
+                shot.setChordNum(self.chordNum)
+                shot.playSounds(self.player)
+                shot.move(self.window.getKeysPressed())
+
+    def updateScreen(self):
+        self.window.clearScreen()
+
+        #-- Background Stars
+        for star in self.backgroundStars:
+            self.window.blitSprite(star)
+
+        #-- Player Star
+        self.window.blitSprite(self.player)
+
+        #-- Pluck Stars
+        for pluck in self.pluckStars:
+            self.window.blitSprite(pluck)
+
+        #-- Anchor Stars
+        for anchor in self.anchorStars:
+            self.window.blitSprite(anchor)
+
+        #-- Shooting Stars
+        for shot in self.shootingStars:
+            if shot.isActive():
+                self.window.blitSprite(shot)
+
+        self.window.updateScreen()
 
 if __name__ == "__main__":
     game = Engine()
